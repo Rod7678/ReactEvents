a813d6f3d04e6ea30a74410c456ad3529f00561b
Rohit <rohitghadge348@gmail.com>
2025-12-30 10:58:55 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header({children}){
+    return (
+        <>
+            <div id="main-header-loading">
+                <header id="main-header">
+                    <div id="header-title">
+                        <h1>React Events</h1>
+                    </div>
+                    <nav>{children}</nav>
+                </header>
+            </div>
+        </>
+    )
+}
